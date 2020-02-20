872708cbca6de32e2abc99222a21449fe6ead151
piglovesyou <thepiglovesyou@gmail.com>
2020-02-20 11:35:54 +0900

    fix

--- test/lib/normalize-new-line.ts
+++ test/lib/normalize-new-line.ts
@@ -1,6 +1,6 @@
 const crlf = /\r\n/g;
 
 export function normalizeNewLine(str: string) {
-  if (crlf.test(str)) str.replace(crlf, '\n');
+  if (crlf.test(str)) return str.replace(crlf, '\n');
   return str;
 }
