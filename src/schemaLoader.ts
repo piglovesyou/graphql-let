d78c340625464c14c86810a5b9b99721f315bfea
piglovesyou <thepiglovesyou@gmail.com>
2020-02-20 12:07:04 +0900

    refac

--- src/schemaLoader.ts
+++ src/schemaLoader.ts
@@ -1,87 +1,72 @@
-import { promises as fsPromises } from 'fs';
 import logUpdate from 'log-update';
 import { loader } from 'webpack';
 import { join as pathJoin } from 'path';
 import { parse as parseYaml } from 'yaml';
 import {
-  processDtsForCodegenContext as _finalizeCodegenContextIfNeeded,
-  prepareFullGenerate as _prepareFullGenerate,
-  processDocuments as _processDocuments,
-  processResolverTypesIfNeeded as _processResolverTypesIfNeeded,
+  processDtsForCodegenContext,
+  prepareFullGenerate,
+  processDocuments,
+  processResolverTypesIfNeeded,
   CodegenContext,
 } from './lib/full-generate';
 import memoize from './lib/memoize';
 import { ConfigTypes } from './lib/types';
 import { DEFAULT_CONFIG_FILENAME } from './lib/consts';
 import { PRINT_PREFIX } from './lib/print';
+import { readFile } from './lib/file';
 
-const { readFile } = fsPromises;
-const prepareFullGenerate = memoize(
-  _prepareFullGenerate,
-  () => 'prepareFullGenerate',
-);
-const processResolverTypesIfNeeded = memoize(
-  _processResolverTypesIfNeeded,
-  () => 'processResolverTypesIfNeeded',
-);
-const processDocuments = memoize(_processDocuments, () => 'processDocuments');
-const finalizeCodegenContextIfNeeded = memoize(
-  _finalizeCodegenContextIfNeeded,
-  () => 'finalizeCodegenContextIfNeeded',
-);
+const processGraphQLCodegenSchemaLoader = memoize(
+  async (addDependency: (path: string) => void, cwd: string) => {
+    const configPath = pathJoin(cwd, DEFAULT_CONFIG_FILENAME);
+    const config = parseYaml(
+      await readFile(configPath, 'utf-8'),
+    ) as ConfigTypes;
 
-const graphlqCodegenSchemaLoader: loader.Loader = function(gqlContent) {
-  const callback = this.async()!;
+    const codegenContext: CodegenContext = [];
 
-  // Wrap them because loader.Loader doesn't expect Promise as the returned value
-  (async () => {
-    try {
-      const { rootContext: cwd } = this;
-      const configPath = pathJoin(cwd, DEFAULT_CONFIG_FILENAME);
-      const config = parseYaml(
-        await readFile(configPath, 'utf-8'),
-      ) as ConfigTypes;
+    const { codegenOpts, gqlRelPaths } = await prepareFullGenerate(config, cwd);
 
-      const codegenContext: CodegenContext = [];
+    const { schemaHash, schemaPaths } = await processResolverTypesIfNeeded(
+      config,
+      cwd,
+      codegenOpts,
+      codegenContext,
+    );
 
-      const { codegenOpts, gqlRelPaths } = await prepareFullGenerate(
-        config,
+    // All documents and schema are dependencies, which change should invalidate caches.
+    gqlRelPaths.forEach(p => addDependency(p));
+    schemaPaths.forEach(p => addDependency(p));
+
+    // Only if schema was changed, documents are also handled for quick startup of webpack dev.
+    if (codegenContext.length) {
+      await processDocuments(
+        gqlRelPaths,
         cwd,
-      );
-
-      const { schemaHash, schemaPaths } = await processResolverTypesIfNeeded(
         config,
-        cwd,
+        schemaHash,
         codegenOpts,
         codegenContext,
       );
 
-      // All documents and schema are dependencies, which change should invalidate caches.
-      gqlRelPaths.forEach(p => this.addDependency(p));
-      schemaPaths.forEach(p => this.addDependency(p));
+      await processDtsForCodegenContext(codegenContext);
+    }
+  },
+  () => 'schemaLoader',
+);
 
-      // Only if schema was changed, documents are also handled for quick startup of webpack dev.
-      if (codegenContext.length) {
-        await processDocuments(
-          gqlRelPaths,
-          cwd,
-          config,
-          schemaHash,
-          codegenOpts,
-          codegenContext,
-        );
+const graphlqCodegenSchemaLoader: loader.Loader = function(gqlContent) {
+  const callback = this.async()!;
+  const { rootContext: cwd } = this;
 
-        await finalizeCodegenContextIfNeeded(codegenContext);
-      }
-
-      // It just passes as it is
+  processGraphQLCodegenSchemaLoader(this.addDependency.bind(this), cwd)
+    .then(() => {
       callback(undefined, gqlContent);
-    } catch (e) {
+    })
+    .catch(e => {
       logUpdate.stderr(PRINT_PREFIX + e.message);
       logUpdate.stderr.done();
       callback(e);
-    }
-  })();
+    });
 };
 
 export default graphlqCodegenSchemaLoader;
