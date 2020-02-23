d2c0192c211059873651047dce58722c9be21d42
piglovesyou <thepiglovesyou@gmail.com>
2020-02-23 23:56:35 +0900

    Add test to check passing config properly
    Fixes #57 #58

--- test/gen.test.ts
+++ test/gen.test.ts
@@ -1,47 +1,66 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 
+import { promises } from 'fs';
 import { join as pathJoin } from 'path';
 import assert from 'assert';
+import _rimraf from 'rimraf';
+import { promisify } from 'util';
 import gen from '../src/gen';
 import glob from 'globby';
-import { promisify } from 'util';
-import _rimraf from 'rimraf';
-import { promises } from 'fs';
+import { normalizeNewLine } from './lib/normalize-new-line';
 
 const rimraf = promisify(_rimraf);
-const { rename } = promises;
-
+const { readFile: _readFile, rename } = promises;
 const cwd = pathJoin(__dirname, 'fixtures/gen');
 const rel = (relPath: string) => pathJoin(cwd, relPath);
 
-describe('"graphql-let" command', () => {
-  beforeAll(async () => await rename(rel('_gitignore'), rel('.gitignore')));
-  afterAll(async () => await rename(rel('.gitignore'), rel('_gitignore')));
+export function readFile(file: string) {
+  return _readFile(file, 'utf-8').then(normalizeNewLine);
+}
 
-  test(
-    `generates .d.ts
+describe('"graphql-let" command', () => {
+  beforeAll(async () => {
+    await rename(rel('_gitignore'), rel('.gitignore'));
+    await rimraf(rel('__generated__'));
+
+    await gen({
+      cwd,
+      configPath: rel('.graphql-let.yml'),
+    });
+  }, 60 * 1000);
+  afterAll(async () => {
+    await rename(rel('.gitignore'), rel('_gitignore'));
+  });
+
+  test(`generates number of .d.ts ignoring specified files as expected
 * ignoring "!" paths in "schema" and "documents" of graphql-let.yml
 * ignoring files specified in .gitignore
-`,
-    async () => {
-      const expectDtsLength = 3; // 2 documents and 1 schema
+`, async () => {
+    const expectDtsLength = 3; // 2 documents and 1 schema
 
-      await rimraf(rel('__generated__'));
-      await gen({
-        cwd,
-        configPath: rel('.graphql-let.yml'),
-      });
+    const globResults = await glob('__generated__/types/**', { cwd });
+    assert.strictEqual(globResults.length, expectDtsLength);
 
-      const globResults = await glob('__generated__/types/**', { cwd });
-      assert.strictEqual(globResults.length, expectDtsLength);
+    const [schema, doc1, doc2] = globResults.sort();
+    const d = '^__generated__/types';
+    const h = '[a-z\\d]+';
+    assert(new RegExp(`${d}/viewer.graphql-${h}.d.ts$`).test(doc1));
+    assert(new RegExp(`${d}/viewer2.graphql-${h}.d.ts$`).test(doc2));
+    assert(new RegExp(`${d}/__concatedschema__-${h}.d.ts$`).test(schema));
+  });
 
-      const [schema, doc1, doc2] = globResults.sort();
-      const d = '^__generated__/types';
-      const h = '[a-z\\d]+';
-      assert(new RegExp(`${d}/viewer.graphql-${h}.d.ts$`).test(doc1));
-      assert(new RegExp(`${d}/viewer2.graphql-${h}.d.ts$`).test(doc2));
-      assert(new RegExp(`${d}/__concatedschema__-${h}.d.ts$`).test(schema));
-    },
-    60 * 1000,
-  );
+  test(`passes config to graphql-codegen as expected
+* "useIndexSignature: true" in config effect to result having "WithIndex<TObject>" type
+`, async () => {
+    const [
+      generatedSchemaTypesPath,
+    ] = await glob('__generated__/types/__concatedschema__*', { cwd });
+    const actual = await readFile(rel(generatedSchemaTypesPath));
+    assert.ok(
+      actual.includes(`
+  export type WithIndex<TObject> = TObject & Record<string, any>;
+  export type ResolversObject<TObject> = WithIndex<TObject>;
+`),
+    );
+  });
 });
