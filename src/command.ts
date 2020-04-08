085b2e6763067bb4e9eb462fb12df7831d884714
Chris Feijoo <chris@kube.io>
2020-04-09 00:12:38 +0200

    Accept path to config file as argument

--- src/command.ts
+++ src/command.ts
@@ -1,12 +1,14 @@
 import minimist from 'minimist';
+import { basename, dirname, resolve } from 'path';
 import { printError } from './lib/print';
 import { CommandOpts } from './lib/types';
 
 const argv = minimist(process.argv.slice(2));
 const HELP_TEXT = `Usage: graphql-let [command]
 
-graphql-let         Generates .graphql.d.ts beside all GraphQL documents based on .graphql-let.yml config
-graphql-let init    Generates a template of .graphql-let.yml configuration file 
+graphql-let                   Generates .graphql.d.ts beside all GraphQL documents based on .graphql-let.yml config
+graphql-let --config [FILE]   Generates .graphql.d.ts given a config file
+graphql-let init              Generates a template of .graphql-let.yml configuration file 
 `;
 
 if (argv.help || argv.h) {
@@ -30,8 +32,15 @@ switch (argv._[0]) {
 }
 
 function createOpts(): CommandOpts {
-  const cwd = process.cwd();
-  return { cwd };
+  if (argv.config) {
+    return {
+      cwd: resolve(process.cwd(), dirname(argv.config)),
+      configFilePath: basename(argv.config),
+    };
+  } else {
+    const cwd = process.cwd();
+    return { cwd };
+  }
 }
 
 function command(command: string) {
