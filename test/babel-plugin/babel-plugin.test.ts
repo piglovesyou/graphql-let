59ea1f5313c7a7e1390e8f574420a158773ac616
Soichi Takamura <thepiglovesyou@gmail.com>
2021-02-11 16:12:40 +0900

    fix: babel-plugin.test.ts

--- test/babel-plugin/babel-plugin.test.ts
+++ test/babel-plugin/babel-plugin.test.ts
@@ -9,6 +9,7 @@ beforeAll(() => cleanup(cwd));
 
 runner(cwd, 'gql', {}, { sourceType: 'unambiguous' });
 
+// Needs "describe", otherwise the order of running another "describe" in @babel/helper-transform-fixture-test-runner switches.
 describe('babel-plugin', () => {
   test(`Generated files are okay`, async () => {
     await matchPathsAndContents(['fixtures/*/node_modules/**'], cwd);
