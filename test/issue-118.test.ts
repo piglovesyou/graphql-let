a874673e64be89378eb1a4c22d05107aa75969f0
Soichi Takamura <thepiglovesyou@gmail.com>
2021-02-12 00:43:47 +0900

    wip: Using prepareFixtures()

--- test/issue-118.test.ts
+++ test/issue-118.test.ts
@@ -1,9 +1,13 @@
-import { join as pathJoin } from 'path';
 import gen from '../src/gen';
+import { AbsFn, prepareFixtures } from './__tools/file';
 
-const cwd = pathJoin(__dirname, '__fixtures/issue-118');
+let cwd: string, abs: AbsFn;
 
 describe('"baseUrl" and "mappers" combo', () => {
+  beforeAll(async () => {
+    [cwd, abs] = await prepareFixtures(__dirname, '__fixtures/issue-118');
+  });
+
   it('should run graphql-let command properly', async () => {
     await gen({ cwd });
   });
