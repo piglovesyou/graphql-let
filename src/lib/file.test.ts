020490b3dd19f7be9271b69c8c23b01b595aaff9
Soichi Takamura <thepiglovesyou@gmail.com>
2021-02-12 08:54:40 +0900

    wip: Use "__fixtures"

--- src/lib/file.test.ts
+++ src/lib/file.test.ts
@@ -5,7 +5,7 @@ import { readHash } from './file';
 describe('file.ts', () => {
   test('readHash reads file from generated file', async () => {
     const hash = await readHash(
-      pathJoin(__dirname, '__fixtures__/file/dummy-graphql-d-ts.txt'),
+      pathJoin(__dirname, '__fixtures/file/dummy-graphql-d-ts.txt'),
     );
     if (!hash) throw new Error('boom');
     strictEqual(hash.length, 40);
