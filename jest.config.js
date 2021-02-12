1121f2a150384b92eb217cbcc65f058e1835aecf
Soichi Takamura <thepiglovesyou@gmail.com>
2021-02-12 22:41:59 +0900

    wip: Use "__tests__"

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   testEnvironment: 'node',
   testTimeout: 60 * 1000,
   globalSetup: '<rootDir>/src/lib/__tools/global-setup.ts',
+  testPathIgnorePatterns: ['/node_modules/', '__fixtures', '.__fixtures'],
   globals: {
     'ts-jest': {
       tsconfig: 'tsconfig.test.json',
