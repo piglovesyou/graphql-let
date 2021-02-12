8c271f79af3fbb05e86d8fb96e200fe8ac0dc19f
Soichi Takamura <thepiglovesyou@gmail.com>
2021-02-12 21:38:43 +0900

    refac babel-plugin.test.ts

--- test/babel-plugin/babel-plugin.test.ts
+++ test/babel-plugin/babel-plugin.test.ts
@@ -1,14 +1,16 @@
 import runner from '@babel/helper-transform-fixture-test-runner';
 import { join } from 'path';
+import { cleanup } from '../__tools/file';
 import { matchPathsAndContents } from '../__tools/match-paths-and-contents';
 
 const cwd = join(__dirname, '__fixtures');
 
+beforeAll(async () => {
+  await cleanup(cwd, ['**/node_modules']);
+});
+
 runner(cwd, 'gql', {}, { sourceType: 'unambiguous' });
 
-// Needs "describe", otherwise the order of running another "describe" in @babel/helper-transform-fixture-test-runner switches.
-describe('babel-plugin', () => {
-  test(`Generated files are okay`, async () => {
-    await matchPathsAndContents(['suite/*/node_modules/**'], cwd);
-  });
+test(`Generated files are okay`, async () => {
+  await matchPathsAndContents(['**/node_modules/**'], cwd);
 });
