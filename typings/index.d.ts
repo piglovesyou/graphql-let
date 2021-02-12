ce91583144c5d320b78c579d20707461df074c73
Soichi Takamura <thepiglovesyou@gmail.com>
2021-02-12 23:15:17 +0900

    Test: why we need this?

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,8 +1,3 @@
 declare module 'terminate';
-declare module 'jest-transform-graphql' {
-  import { Transformer } from '@jest/transform';
-  const transformer: Transformer;
-  export default transformer;
-}
 declare module '@babel/helper-transform-fixture-test-runner';
 declare module '@babel/helper-plugin-utils';
