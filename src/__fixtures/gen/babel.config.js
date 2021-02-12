a4c055e6eaa47c70c49437cc8231560a1ed9b0e8
Soichi Takamura <thepiglovesyou@gmail.com>
2021-02-12 19:38:02 +0900

    Rename to "babel-plugin.ts"

--- src/__fixtures/gen/babel.config.js
+++ src/__fixtures/gen/babel.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: [
-    "@babel/plugin-syntax-jsx",
-    ["../../../src/babel.ts", { importName: 'graphql-let' }],
-  ]
+    '@babel/plugin-syntax-jsx',
+    ['../../../babel.js', { importName: 'graphql-let' }],
+  ],
 };
