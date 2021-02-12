eb7dc7846203c1197b755ec608d2fae7428cec1c
Soichi Takamura <thepiglovesyou@gmail.com>
2021-02-12 09:52:45 +0900

    wip: Make tsconfig simple and explicit

---
+++ src/__tests/babel-plugin/__fixtures/suite/tagged-template-call/output.mjs
@@ -0,0 +1,4 @@
+import * as V98fe64b5c1bccab885117fbeac3ea597d4f7e02d from "graphql-let/__generated__/input-98fe64b5c1bccab885117fbeac3ea597d4f7e02d.tsx";
+const {
+  useViewerQuery
+} = V98fe64b5c1bccab885117fbeac3ea597d4f7e02d;
