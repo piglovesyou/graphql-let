7627a9ee1f7d7f03d3b15e9720f1623f9fd28b7e
Soichi Takamura <thepiglovesyou@gmail.com>
2021-02-13 22:31:47 +0900

    Check types of babel-plugin results
    * tagged template literal can not be typed by us

--- src/babel-plugin.test.ts
+++ src/babel-plugin.test.ts
@@ -1,5 +1,7 @@
 import runner from '@babel/helper-transform-fixture-test-runner';
+import glob from 'globby';
 import { join } from 'path';
+import { spawn } from './lib/__tools/child-process';
 import { cleanup } from './lib/__tools/file';
 import { matchPathsAndContents } from './lib/__tools/match-paths-and-contents';
 
@@ -10,6 +12,15 @@ beforeAll(async () => {
 });
 
 runner(cwd, 'gql', {}, { sourceType: 'unambiguous' });
+
+test(`Type checking for all fixtures`, async () => {
+  const dirs = await glob(['*/*'], {
+    cwd,
+    onlyDirectories: true,
+    absolute: true,
+  });
+  for (const dir of dirs) await spawn('yarn', ['tsc'], { cwd: dir });
+});
 
 test(`Generated files are okay`, async () => {
   await matchPathsAndContents(['**/node_modules/**'], cwd);
