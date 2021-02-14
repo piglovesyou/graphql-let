c3a189bdfe03635288c1a09eab8a581a68198746
Soichi Takamura <thepiglovesyou@gmail.com>
2021-02-14 19:00:33 +0900

    wip: Rename to babel.test.ts

--- src/babel.test.ts
+++ src/babel.test.ts
@@ -3,8 +3,9 @@ import glob from 'globby';
 import { join } from 'path';
 import { spawn } from './lib/__tools/child-process';
 import { cleanup } from './lib/__tools/file';
+import { matchPathsAndContents } from './lib/__tools/match-paths-and-contents';
 
-const cwd = join(__dirname, '.__fixtures/macro');
+const cwd = join(__dirname, '.__fixtures/babel');
 
 beforeAll(async () => {
   await cleanup(cwd, ['**/node_modules']);
@@ -21,6 +22,6 @@ test(`Type checking for all fixtures`, async () => {
   for (const dir of dirs) await spawn('yarn', ['tsc'], { cwd: dir });
 });
 
-// test(`Generated files are okay`, async () => {
-//   await matchPathsAndContents(['**/node_modules/**'], cwd);
-// });
+test(`Generated files are okay`, async () => {
+  await matchPathsAndContents(['**/node_modules/**'], cwd);
+});
--- src/lib/__tools/global-setup.ts
+++ src/lib/__tools/global-setup.ts
@@ -7,5 +7,5 @@ export default async function setup() {
   await cleanup(projRoot, ['**/.__fixtures']);
 
   // XXX: Hacky. @babel/helper-transform-fixture-test-runner wants this.
-  await prepareFixtures(join(__dirname, '../../'), '__fixtures/babel-plugin');
+  await prepareFixtures(join(__dirname, '../../'), '__fixtures/babel');
 }
