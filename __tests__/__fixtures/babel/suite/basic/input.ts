d31c20318494bab168a1a86a4d6d7f34d8dd9f91
Soichi Takamura <thepiglovesyou@gmail.com>
2021-02-15 21:01:22 +0900

    Unsupport babel-plugin
    And go only with babel-plugin-macros

--- __tests__/__fixtures/babel/suite/basic/input.ts
+++
@@ -1,6 +0,0 @@
-import gql from 'graphql-let';
-const { useViewerQuery } = gql('query Viewer { viewer { name } }');
-
-useViewerQuery().data.viewer.name as string;
-// @ts-expect-error
-useViewerQuery() as number;
