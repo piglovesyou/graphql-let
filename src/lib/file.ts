5a43b4a962400e92d6718dc6bac69c091c293602
Soichi Takamura <thepiglovesyou@gmail.com>
2021-03-05 13:50:23 +0900

    Move rimraf to devDeps

--- src/lib/file.ts
+++ src/lib/file.ts
@@ -1,8 +1,4 @@
 import { promises as fsPromises } from 'fs';
-import _rimraf from 'rimraf';
-import { promisify } from 'util';
-
-export const rimraf = promisify(_rimraf);
 
 export const { readFile, writeFile, unlink } = fsPromises;
 export { exists, readFileSync, statSync } from 'fs';
