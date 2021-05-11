cdbcd83fc8f0e16bd33fbc988f75ee94db889a80
Soichi Takamura <thepiglovesyou@gmail.com>
2021-05-11 21:27:48 +0900

    wip: pass codegen.test.ts

--- src/lib/codegen.test.ts
+++ src/lib/codegen.test.ts
@@ -1,9 +1,8 @@
 import { join as pathJoin } from 'path';
 import { processCodegenForContext } from './codegen';
 import loadConfig from './config';
-import createExecContext from './exec-context';
-import { createPaths, createSchemaPaths } from './paths';
-import { CodegenContext } from './types';
+import { createExecContext } from './exec-context';
+import { createPaths } from './paths';
 import { matchPathsAndContents } from './__tools/match-paths-and-contents';
 
 const cwd = pathJoin(__dirname, '__fixtures/graphql-codegen');
@@ -12,21 +11,17 @@ describe('graphql-codegen.ts', () => {
   test('works', async () => {
     const [config, configHash] = await loadConfig(cwd);
 
-    const execContext = createExecContext(cwd, config, configHash);
-    const codegenContext: CodegenContext[] = [
-      {
-        ...createSchemaPaths(execContext),
-        type: 'schema-import',
-        gqlHash: 'xx',
-        skip: false,
-      },
-      {
-        ...createPaths(execContext, 'pages/viewer.graphql'),
-        type: 'document-import',
-        gqlHash: 'xx',
-        skip: false,
-      },
-    ];
+    const { execContext, codegenContext } = await createExecContext(
+      cwd,
+      config,
+      configHash,
+    );
+    codegenContext.push({
+      ...createPaths(execContext, 'pages/viewer.graphql'),
+      type: 'document-import',
+      gqlHash: 'xx',
+      skip: false,
+    });
     await processCodegenForContext(execContext, codegenContext);
     await matchPathsAndContents(['__generated__'], cwd);
   });
