597b654cffe23919b97ab573fe71f043abe92468
Soichi Takamura <thepiglovesyou@gmail.com>
2021-05-28 08:55:19 +0900

    Test if types of a query variable works
    It seems working correctly.
    Ref. #495

--- src/__fixtures/gen/1_basic/pages/index.tsx
+++ src/__fixtures/gen/1_basic/pages/index.tsx
@@ -1,6 +1,14 @@
 import { useViewerQuery } from './viewer.graphql';
 
-const result = useViewerQuery();
+// @ts-expect-error
+useViewerQuery();
+
+// @ts-expect-error
+useViewerQuery({ variables: {} });
+
+const result = useViewerQuery({
+  variables: { id: 'baa' },
+});
 result.data.viewer.status as string;
 // @ts-expect-error
 result.data.viewer.status as number;
