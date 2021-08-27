8ed1424e9553f7d398a0dfe1154b0b4e35cbeee1
Vadym Okun <vadym.okun@rmgmedia.com>
2021-08-28 02:35:06 +0300

    Slight adjustments of codestyle and arguments rename

--- src/jestTransformer.ts
+++ src/jestTransformer.ts
@@ -21,16 +21,14 @@ function getOption(jestConfig: ProjectConfig): JestTransformerOptions {
 }
 
 const jestTransformer: Transformer<JestTransformerOptions> = {
-  getCacheKey(fileData, filename, configString) {
-    return createHash(fileData + filename + configString + 'graphql-let');
+  getCacheKey(sourceText, sourcePath, options) {
+    const configString = options?.configString || options;
+
+    return createHash(sourceText + sourcePath + configString + 'graphql-let');
   },
-  process(
-    input,
-    filePath,
-    __compatJestConfig,
-    __compatTransformOptions?: unknown,
-  ) {
+  process(sourceText, sourcePath, ...rest) {
     // jest v26 vs v27 changes to support both formats: start
+    const [__compatJestConfig] = rest;
     const jestConfig = __compatJestConfig?.config ?? __compatJestConfig;
     // jest v26 vs v27 changes to support both formats: end
     const { rootDir: cwd } = jestConfig;
@@ -38,7 +36,7 @@ const jestTransformer: Transformer<JestTransformerOptions> = {
     const [config, configHash] = loadConfigSync(cwd, configFile);
     const { execContext } = createExecContextSync(cwd, config, configHash);
 
-    const { tsxFullPath } = createPaths(execContext, relative(cwd, filePath));
+    const { tsxFullPath } = createPaths(execContext, relative(cwd, sourcePath));
     const tsxContent = readFileSync(tsxFullPath, 'utf-8');
 
     // Let users customize a subsequent transformer
@@ -47,25 +45,17 @@ const jestTransformer: Transformer<JestTransformerOptions> = {
       const _subsequentTransformer = require(subsequentTransformer);
       return (
         _subsequentTransformer?.default || _subsequentTransformer
-      ).process(
-        tsxContent,
-        tsxFullPath,
-        __compatJestConfig,
-        __compatTransformOptions,
-      );
+      ).process(tsxContent, tsxFullPath, ...rest);
     }
 
+    // jest v26 vs v27 changes to support both formats: start
     // "babel-jest" by default
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     const babelJest = require('babel-jest');
     const { createTransformer } = babelJest?.default || babelJest;
+    // jest v26 vs v27 changes to support both formats: end
     const babelTransformer = createTransformer({ cwd: cwd });
-    return babelTransformer.process(
-      tsxContent,
-      tsxFullPath,
-      __compatJestConfig,
-      __compatTransformOptions,
-    );
+    return babelTransformer.process(tsxContent, tsxFullPath, ...rest);
   },
 };
 
