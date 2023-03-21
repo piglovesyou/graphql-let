f23ae30b33ab19a23297cfc1ff2c4320f1711f77
Charlie Croom <charlie.croom@cityblock.com>
2023-03-21 12:04:16 -0400

    Make Schema processing match graphql-codegen for consistent behavior

--- src/lib/exec-context.ts
+++ src/lib/exec-context.ts
@@ -1,4 +1,16 @@
 import { Types } from '@graphql-codegen/plugin-helpers';
+import { ApolloEngineLoader } from '@graphql-tools/apollo-engine-loader';
+import { CodeFileLoader } from '@graphql-tools/code-file-loader';
+import { GitLoader } from '@graphql-tools/git-loader';
+import { GithubLoader } from '@graphql-tools/github-loader';
+import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
+import { JsonFileLoader } from '@graphql-tools/json-file-loader';
+import {
+  loadSchemaSync,
+} from '@graphql-tools/load';
+import { printSchema } from 'graphql';
+import { PrismaLoader } from '@graphql-tools/prisma-loader';
+import { UrlLoader } from '@graphql-tools/url-loader';
 import { readFileSync } from 'fs';
 import gensync from 'gensync';
 import slash from 'slash';
@@ -29,26 +41,32 @@ function getSchemaPointers(
   return _acc;
 }
 
-function prepareCreateSchemaHashArgs(execContext: ExecContext) {
+const createSchemaHashGenerator = gensync(function* (execContext: ExecContext) {
   const { config, configHash, cwd } = execContext;
   const schemaPointers = getSchemaPointers(config.schema!);
-  // TODO: How can we detect update of remote GraphQL Schema? ETag?
-  // It caches the remote introspection forever in the current implementation.
-  const filePointers = schemaPointers.filter((p) => !isURL(p));
-  return { configHash, cwd, filePointers };
-}
 
-const createSchemaHashGenerator = gensync(function* (execContext: ExecContext) {
-  const { configHash, cwd, filePointers } = prepareCreateSchemaHashArgs(
-    execContext,
-  );
+  // Uses the same loading mechanism as the underlying graphql-codegen
+  // See https://github.com/dotansimha/graphql-code-generator/blob/master/packages/graphql-codegen-cli/src/load.ts
+  const loaders = [
+    new CodeFileLoader(),
+    new GitLoader(),
+    new GithubLoader(),
+    new GraphQLFileLoader(),
+    new JsonFileLoader(),
+    new UrlLoader(),
+    new ApolloEngineLoader(),
+    new PrismaLoader(),
+  ];
 
-  const files: string[] = yield* globby(filePointers, { cwd, absolute: true });
-  const contents = files
-    .map(slash)
-    .sort()
-    .map((file) => readFileSync(file, 'utf-8'));
-  return createHashFromBuffers([configHash, ...contents]);
+  const schema = printSchema(loadSchemaSync(schemaPointers, {
+      cwd,
+      loaders: [ 
+        new CodeFileLoader(), 
+        new GraphQLFileLoader() 
+      ]
+  }));
+
+  return createHashFromBuffers([configHash, schema]);
 });
 
 const appendSchemaImportContextGenerator = gensync(function* (
